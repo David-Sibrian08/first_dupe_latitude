3f99f04bf3c1309c1056ee5c1e3076a56cc4dc89
David-Sibrian08 <davidsibrian08@gmail.com>
2023-02-10 23:29:28 -0500

    main class done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,42 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.HashSet;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] input = parseInput(args);
+        int dupe = findFirstDuplicate(input);
+        System.out.println(dupe);
+    }
+
+    private static int[] parseInput(String[] args) {
+        int[] elements = new int[args.length];
+        for (int i = 0; i<args.length; i++) {
+            elements[i] = Integer.parseInt(args[i]);
+        }
+        return elements;
+    }
+
+    public static int findFirstDuplicate(int[] numbers) {
+        int count = numbers.length;
+
+        if (numbers == null) {
+            return -1;
+        }
+
+        if (count == 0) {
+            return -1;
+        }
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int x= 0; x < count; x++) {
+            if(seen.contains(numbers[x])) {
+                return numbers[x];
+            } else {
+                seen.add(numbers[x]);
+            }
+        }
+        return -1;
     }
 }
